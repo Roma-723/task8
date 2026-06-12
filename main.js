8b4684f5f6f7eb2d56583171fce8d15173f3d0a4
Roma-723 <romajon.ll8@gmail.com>
2026-06-12 09:18:55 +0500

    firsName

--- main.js
+++ main.js
@@ -21,19 +21,19 @@
                                                                                        
 // function deepEqual(a, b) {                                                         
 //   return Object.values(a).toString() === Object.values(b).toString();             
-// }                                                                                //
-                                                                                   //
-                                                                                  //
-function deepEqual(a, b) {                                                       //
-  if (typeof a !== typeof b) return false;                                      //
-  if (typeof a !== "object" || a === null || b === null) return a === b;       //
-                                                                              //
-  const keysA = Object.keys(a);                                              //
-  const keysB = Object.keys(b);                                             //
-  if (keysA.length !== keysB.length) return false;                         // 
-  return keysA.every(key => deepEqual(a[key], b[key]));                   //
-}                                                                        // 
-console.log(deepEqual({ a: 1, b: { c: 2 } }, { a: 1, b: { c: 2 } }));   // 
-console.log(deepEqual({ a: 1 }, { a: "1" }));                          //
-console.log(deepEqual([1, 2, [3]], [1, 2, [3]]));                     //
+// }                                                                                
+                                                                                  
+                                                                                  
+function deepEqual(a, b) {                                                       
+  if (typeof a !== typeof b) return false;                                      
+  if (typeof a !== "object" || a === null || b === null) return a === b;      
+                                                                              
+  const keysA = Object.keys(a);                                              
+  const keysB = Object.keys(b);                                             
+  if (keysA.length !== keysB.length) return false;                          
+  return keysA.every(key => deepEqual(a[key], b[key]));                   
+}                                                                        
+console.log(deepEqual({ a: 1, b: { c: 2 } }, { a: 1, b: { c: 2 } }));    
+console.log(deepEqual({ a: 1 }, { a: "1" }));                          
+console.log(deepEqual([1, 2, [3]], [1, 2, [3]]));                     
  
