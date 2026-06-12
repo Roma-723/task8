f0a0a40eb22f3dac344c801617b9fcae1a67ecc5
Roma-723 <romajon.ll8@gmail.com>
2026-06-12 09:10:44 +0500

    firsName

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
                                                                                                     
 // // }                                                                                            
 // // console.log(({ a: 1, b: { c: 2 } }, { a: 1, b: { c: 2 } }));                                
-                                                                                                 //
+                                                                                                 
                                                                                                 //
 // function deepEqual(a, b) {                                                                  //
 //     return Object.values(a).toString() === Object.values(b).toString();                    //
