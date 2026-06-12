b553e05ced031377e6f5e33b2208ca3c1be1d7ff
Roma-723 <romajon.ll8@gmail.com>
2026-06-12 11:45:17 +0500

    firsName

--- main.js
+++ main.js
@@ -21,7 +21,6 @@
 //   return Object.values(a).toString() === Object.values(b).toString();             
 // }                                                                                
                                                                                   
-                                                                                  
 function deepEqual(a, b) {                                                       
   if (typeof a !== typeof b) return false;                                      
   if (typeof a !== "object" || a === null || b === null) return a === b;      
@@ -31,6 +30,7 @@ function deepEqual(a, b) {
   if (keysA.length !== keysB.length) return false;                          
   return keysA.every(key => deepEqual(a[key], b[key]));                   
 }  
+
 console.log(deepEqual({ a: 1, b: { c: 2 } }, { a: 1, b: { c: 2 } }));    
 console.log(deepEqual({ a: 1 }, { a: "1" }));                          
 console.log(deepEqual([1, 2, [3]], [1, 2, [3]])); 
